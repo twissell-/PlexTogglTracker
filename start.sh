ebba68b431323ae199ceb232b4c67af295441d03
twissell- <dmaggioesne@gmail.com>
2023-06-22 05:23:50 +0200

    Fixed start.sh script to actually work when cronned

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -eu
 set -o pipefail
 
-# A minimal script to cron on my seedbox to ensure the flask server is always running.
+# A minimal script to cron and ensure the flask server is always running.
+#
+# # rin
+# * * * * * /home/twissell/bin/rin/start.sh
+# 0 0 * * * [ -f /home/twissell/bin/rin/screen/log ] && rm /home/twissell/bin/rin/screen/log
 
-start_command="screen -t rin -d -m /home/twissell/bin/rin/venv/bin/python /home/twissell/bin/rin/run.py"
+RIN_DIR="/home/twissell/bin/rin"
+start_command="screen -L -Logfile ${RIN_DIR}/screen/log -dmS rin ${RIN_DIR}/venv/bin/python ${RIN_DIR}/run.py"
 
 if ! pgrep -if "${start_command}" &> /dev/null; then
+    cd "${RIN_DIR}"
+    export SCREENDIR="${RIN_DIR}/screen"
     $start_command
 fi
